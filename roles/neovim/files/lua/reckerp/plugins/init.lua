530712a043ba8ceb0f9125af0368ffe336f8a68e
reckerp <git@recker.io>
2024-04-02 17:26:14 +0200

    feat(nvim): add platform io plugin

--- roles/neovim/files/lua/reckerp/plugins/init.lua
+++ roles/neovim/files/lua/reckerp/plugins/init.lua
@@ -66,4 +66,12 @@ return {
       vim.keymap.set('n', '<leader>mb', ':MarkdownPreview github<CR>')
     end,
   },
+  {
+    'anurag3301/nvim-platformio.lua',
+    dependencies = {
+      { 'akinsho/nvim-toggleterm.lua' },
+      { 'nvim-telescope/telescope.nvim' },
+      { 'nvim-lua/plenary.nvim' },
+    },
+  },
 }
