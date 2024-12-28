5fb93692edd9a942a5b8525606b7b98e846f8d0a
reckerp <paul@mintano.com>
2024-12-28 17:27:16 +0100

    feat: add file path

--- roles/neovim/files/lua/reckerp/lazy/telescope.lua
+++ roles/neovim/files/lua/reckerp/lazy/telescope.lua
@@ -59,7 +59,11 @@ return { -- Fuzzy Finder (files, lsp, etc)
       --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
       --   },
       -- },
-      -- pickers = {}
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+      },
       extensions = {
         ['ui-select'] = { require('telescope.themes').get_dropdown() },
         bibtex = {
