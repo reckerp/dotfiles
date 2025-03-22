8301692ab988fe3fbb1b43cea15519f214703d9e
reckerp <paul@mintano.com>
2025-03-22 13:37:49 +0100

    feat: update options, flutter config, rm avante

--- roles/neovim/files/lua/config/options.lua
+++ roles/neovim/files/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+
+-- Disable horizontal scrolling
+vim.opt.mousescroll = "ver:3,hor:0"
+
+-- Map horizontal scroll wheel to no operation
+vim.keymap.set("", "<ScrollWheelLeft>", "<Nop>", { silent = true })
+vim.keymap.set("", "<ScrollWheelRight>", "<Nop>", { silent = true })
